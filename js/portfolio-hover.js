de097acceb782c3ab645dd32d726cee252d3eda3
MAC <mac@AlexBabinMac.local>
2022-12-29 17:07:13 +0500

    Add button for portfolio

--- js/portfolio-hover.js
+++ js/portfolio-hover.js
@@ -3,7 +3,7 @@ let contentMocup = document.querySelector('.slide-content__mockup');
 let content = document.querySelector('.slide-content');
 let frame = document.querySelector('.slide-frame');
 let pageTitle = document.querySelector('.portfolio-title'); 
-
+let slideLabel = document.querySelector('.slide-label'); 
 
 frameMocup.addEventListener('mouseover', removeClass);
 contentMocup.addEventListener('mouseout', addClass);
@@ -12,11 +12,13 @@ function removeClass() {
     content.classList.remove('visually-hidden');
     frame.classList.add ('visually-hidden');
     pageTitle.classList.add ('visually-hidden');
+    slideLabel.classList.add ('slide-label--onhover');
 }
 
 function addClass() {
     content.classList.add('visually-hidden');
     frame.classList.remove ('visually-hidden');
     pageTitle.classList.remove ('visually-hidden');
+    slideLabel.classList.remove ('slide-label--onhover');
 }
 
