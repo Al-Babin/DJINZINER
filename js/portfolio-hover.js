2bed29283e696c8ca801841cdf8ab1418b7e2764
MAC <mac@AlexBabinMac.local>
2023-01-14 16:37:08 +0500

    Fixed hovering on button at portfolio when hover the slide

--- js/portfolio-hover.js
+++ js/portfolio-hover.js
@@ -1,5 +1,5 @@
 let frameMocup = document.querySelectorAll('.slide-frame__mockup');
-let contentMocup = document.querySelectorAll('.slide-content__mockup');
+let slideContentWrapper = document.querySelectorAll('.slide-content__wrapper');
 let content = document.querySelectorAll('.slide-content');
 let frame = document.querySelectorAll('.slide-frame');
 let pageTitle = document.querySelector('.portfolio-title'); 
@@ -7,11 +7,10 @@ let slideLabel = document.querySelectorAll('.slide-label');
 let slideCollection = document.querySelectorAll('.slide'); 
 let arrowButtons = document.querySelectorAll('.arrow-button');
 
-
 //adding listeners on all slides
 for(let i = 0; i < frameMocup.length; i++) {
-    frameMocup[i].addEventListener('mouseover', removeClass);
-    contentMocup[i].addEventListener('mouseout', addClass);     
+    frameMocup[i].addEventListener('mouseenter', removeClass);
+    slideContentWrapper[i].addEventListener('mouseleave', addClass);     
 }
 
 function removeClass() {
