80d28bac90f4d4565978727c937d90b4d8ebede3
MAC <mac@AlexBabinMac.local>
2023-01-24 01:15:06 +0500

    Add validation to form and fixed logic of hidding labels

--- js/show-form.js
+++ js/show-form.js
@@ -2,6 +2,8 @@ const priceButton = document.querySelector('.price-button');
 const form = document.querySelector('.form');
 const feedbackFormBlock = document.querySelector('.feedback-form');
 const feedbackFormBg = document.querySelector('.feedback-form__bg');
+const inputs = document.querySelectorAll('.form-field__input');
+const placeholders = document.querySelectorAll('.form-field__placeholder');
 
 
 priceButton.addEventListener('click', showForm);
@@ -10,13 +12,20 @@ feedbackFormBlock.addEventListener('click', (e) => {
     if ( ! withinBoundaries ) {
         hideForm();
         setTimeout(() => {
-            feedbackFormBlock.classList.add('visually-hidden')
+            feedbackFormBlock.classList.add('visually-hidden');
+            //Clear inputs when hideform
+            for(let input of inputs) {
+                input.value = "";
+            }
+            for(let placeholder of placeholders) {
+                placeholder.removeAttribute('style'); 
+            }
         }, 600);
     }
 });
 
 document.addEventListener('keydown', function(e) {
-	if( e.code == "Escape" ){ // код клавиши Escape, но можно использовать e.key
+	if( e.code == "Escape" ){ 
         hideForm();
         setTimeout(() => {
             feedbackFormBlock.classList.add('visually-hidden')
--- js/validate-form.js
+++ js/validate-form.js
@@ -1,1 +1,16 @@
-const placeholder = document.querySelectorAll('.form-field__placeholder');
+const placeholders = document.querySelectorAll('.form-field__placeholder');
+const inputs = document.querySelectorAll('.form-field__input');
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].onfocus = function() {
+        placeholders[i].style.opacity = 0;  
+    }
+
+    inputs[i].onblur = function() {
+        if(!inputs[i].value.trim()){
+            inputs[i].value = "";
+            placeholders[i].removeAttribute('style'); 
+        }
+    }
+}
+
